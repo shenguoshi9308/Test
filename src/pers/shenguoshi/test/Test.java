81b55728c2f4918d3cb36accca522d4637b508dd
shenguoshi9308 <shenguoshi9308@gmail.com>
2016-09-26 16:50:49 +0800

    first commit

---
+++ src/pers/shenguoshi/test/Test.java
@@ -0,0 +1,12 @@
+package pers.shenguoshi.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//20160926
+		System.out.println("Hello World");
+
+	}
+
+}
